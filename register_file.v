015f2f46e7ec24ef83e6e2d56826ca9c00be9b20
Haridarshan B Kunder <darshanhari86@gmail.com>
2026-08-01 11:46:05 +0530

    Update register_file.v

--- register_file.v
+++ register_file.v
@@ -1,43 +1,42 @@
 module register_file(
-    input clk,
-    input we,
+ input clk,
+input we,
 
-    input [1:0] read_addr1,
-    input [1:0] read_addr2,
+input [1:0] read_addr1,
+input [1:0] read_addr2,
 
-    input [1:0] write_addr,
-    input [3:0] write_data,
+input [1:0] write_addr,
+input [3:0] write_data,
 
-    output [3:0] read_data1,
-    output [3:0] read_data2
+output [3:0] read_data1,
+output [3:0] read_data2
 );
 
     // Four 4-bit registers
-    reg [3:0] regfile [0:3];
+reg [3:0] regfile [0:3];
 
     //--------------------------------------------------
     // Initialize registers
     //--------------------------------------------------
-    initial begin
+initial begin
         regfile[0] = 4'd1;   // R0 = 1
         regfile[1] = 4'd2;   // R1 = 2
         regfile[2] = 4'd0;   // R2 = 0
         regfile[3] = 4'd0;   // R3 = 0
-    end
+end
 
     //--------------------------------------------------
     // Asynchronous Read
     //--------------------------------------------------
-    assign read_data1 = regfile[read_addr1];
-    assign read_data2 = regfile[read_addr2];
+assign read_data1 = regfile[read_addr1];
+assign read_data2 = regfile[read_addr2];
 
     //--------------------------------------------------
     // Synchronous Write
     //--------------------------------------------------
-    always @(posedge clk)
-    begin
-        if (we)
-            regfile[write_addr] <= write_data;
-    end
-
+always @(posedge clk)
+begin
+if (we)
+regfile[write_addr] <= write_data;
+end
 endmodule
